500011d36f4c7ba64efb9e07d11b820d582de9e4
amilkey1 <analisa.milkey@gmail.com>
2025-07-16 14:38:04 -0400

    added untracked files

---
+++ main-template/ruv.py
@@ -0,0 +1,13 @@
+import sys, re, os, subprocess as sub
+
+nreps = __NREPS__
+for rep in range(nreps):
+	rep_plus_one = rep + 1
+#	print('Replicate %d of %d...' % (rep_plus_one,nreps))
+
+	# Extract rank
+	fn = 'rep%d/smc/rank.txt' % rep_plus_one
+	stuff = open(fn, 'r').read()
+	m = re.search(r'rank: (?P<rank>[.0-9]+)', stuff, re.M | re.S)
+	rank = float(m.group('rank'))
+	print(rank, ",")
