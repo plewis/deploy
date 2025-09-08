60f6ef1c41e98b2a6765a4f4dc5d0c73778de289
amilkey1 <84950190+amilkey1@users.noreply.github.com>
2025-09-08 10:14:03 -0400

    Create summarize-bhv-info.py

---
+++ main-template/summarize-bhv-info.py
@@ -0,0 +1,212 @@
+import sys, re, os, subprocess as sub
+
+plot_theta_vs_lambda = __PLOT_THETA_VS_LAMBDA__
+nspecies = __NUM_SPECIES__
+
+data_from_file = False
+if len(sys.argv) > 1:
+    data_from_file = True
+    
+    
+summary = []
+
+
+print('Creating BHV info plot...')
+
+nreps = __NREPS__
+for rep in range(nreps):
+  rep_plus_one = rep + 1
+          
+  if __AAM21005__:
+      # extract deep coalescences
+      fn = 'g-prior/rep%d/sim/deep_coalescences.txt' % rep_plus_one
+      maxdeep = 0
+      stuff = open(fn, 'r').read()
+      # Extract numdeep, maxdeep, and stoheight
+      m = re.search(r'num deep coalescences = (?P<numdeep>\d+)\s+Maximum number of deep coalescences = (?P<maxdeep>\d+)\s+True species tree height = (?P<stoheight>[.0-9]+)', stuff, re.M | re.S)
+      assert m is not None, 'could not extract deep coalescences from file "%s"' % fn
+      numdeep = int(m.group('numdeep'))
+      maxdeep = int(m.group('maxdeep'))
+      stoheight = float(m.group('stoheight'))
+      
+       # Extract stxheight (species tree expected height)
+      m = re.search(r'Expected species tree height = (?P<stxheight>[.0-9]+)', stuff, re.M | re.S)
+      assert m is not None, 'could not extract expected species tree height from file "%s"' % fn
+      stxheight = float(m.group('stxheight'))
+      
+      # extract lambda and theta mean
+      fn = 'rep%d/sim/proj.conf' % rep_plus_one
+      stuff = open(fn, 'r').read()
+      m = re.search(r'theta\s+=\s+(?P<theta>[-.e0-9]+)\s+lambda\s+=\s+(?P<lambda>[.e0-9]+)', stuff, re.M | re.S)
+      assert m is not None, 'could not extract theta and lambda from file "%s"' % fn
+      theta = float(m.group('theta'))
+      lamBda = float(m.group('lambda'))
+      
+     # Extract information from BHV info output
+      fn = 'info.txt'
+      stuff_two = open(fn, 'r').read()
+
+     # Extract means from BHV output
+      fn = 'g-posterior/rep%d/sim/proj.conf' % rep_plus_one
+      stuff = open(fn, 'r').read()
+      m = re.search(r'theta\s+=\s+(?P<theta>[-.e0-9]+)\s+lambda\s+=\s+(?P<lambda>[.e0-9]+)', stuff, re.M | re.S)
+      assert m is not None, 'could not extract theta and lambda from file "%s"' % fn
+      theta = float(m.group('theta'))
+      
+      
+  elif __JJC23002__:
+            # extract deep coalescences
+      fn = 'g-prior/rep%d/sim/deep_coalescences.txt' % rep_plus_one
+      maxdeep = 0
+      stuff = open(fn, 'r').read()
+      # Extract numdeep, maxdeep, and stoheight
+      m = re.search(r'num deep coalescences = (?P<numdeep>\d+)\s+Maximum number of deep coalescences = (?P<maxdeep>\d+)\s+True species tree height = (?P<stoheight>[.0-9]+)', stuff, re.M | re.S)
+      assert m is not None, 'could not extract deep coalescences from file "%s"' % fn
+      numdeep = int(m.group('numdeep'))
+      maxdeep = int(m.group('maxdeep'))
+      stoheight = float(m.group('stoheight'))
+      
+       # Extract stxheight (species tree expected height)
+      m = re.search(r'Expected species tree height = (?P<stxheight>[.0-9]+)', stuff, re.M | re.S)
+      assert m is not None, 'could not extract expected species tree height from file "%s"' % fn
+      stxheight = float(m.group('stxheight'))
+      
+      # extract lambda and theta mean
+      fn = 'rep%d/sim/proj.conf' % rep_plus_one
+      stuff = open(fn, 'r').read()
+      m = re.search(r'theta\s+=\s+(?P<theta>[-.e0-9]+)\s+lambda\s+=\s+(?P<lambda>[.e0-9]+)', stuff, re.M | re.S)
+      assert m is not None, 'could not extract theta and lambda from file "%s"' % fn
+      theta = float(m.group('theta'))
+      lamBda = float(m.group('lambda'))
+      
+     # Extract information from BHV info output
+      fn = 'info.txt'
+      stuff_two = open(fn, 'r').read()
+                  
+  elif __POL02003__:
+            # extract deep coalescences
+      fn = 'g-prior/rep%d/sim/deep_coalescences.txt' % rep_plus_one
+      maxdeep = 0
+      stuff = open(fn, 'r').read()
+      # Extract numdeep, maxdeep, and stoheight
+      m = re.search(r'num deep coalescences = (?P<numdeep>\d+)\s+Maximum number of deep coalescences = (?P<maxdeep>\d+)\s+True species tree height = (?P<stoheight>[.0-9]+)', stuff, re.M | re.S)
+      assert m is not None, 'could not extract deep coalescences from file "%s"' % fn
+      numdeep = int(m.group('numdeep'))
+      maxdeep = int(m.group('maxdeep'))
+      stoheight = float(m.group('stoheight'))
+      
+       # Extract stxheight (species tree expected height)
+      m = re.search(r'Expected species tree height = (?P<stxheight>[.0-9]+)', stuff, re.M | re.S)
+      assert m is not None, 'could not extract expected species tree height from file "%s"' % fn
+      stxheight = float(m.group('stxheight'))
+      
+      # extract lambda and theta mean
+      fn = 'rep%d/sim/proj.conf' % rep_plus_one
+      stuff = open(fn, 'r').read()
+      m = re.search(r'theta\s+=\s+(?P<theta>[-.e0-9]+)\s+lambda\s+=\s+(?P<lambda>[.e0-9]+)', stuff, re.M | re.S)
+      assert m is not None, 'could not extract theta and lambda from file "%s"' % fn
+      theta = float(m.group('theta'))
+      lamBda = float(m.group('lambda'))
+      
+     # Extract information from BHV info output
+      fn = 'info.txt'
+      stuff_two = open(fn, 'r').read()
+  
+ # Extract SMC RF means
+  fn = 'smcrf%d.txt' % rep_plus_one
+  assert (os.path.exists(fn))
+  lines = open(fn, 'r').readlines()
+  smcrfsum = 0.0
+  smcrfnum = 0
+  for line in lines[1:]:
+      parts = line.strip().split()
+      assert len(parts) == 2, 'expecting 2 parts but found %d in file "%s"' % (len(parts), fn)
+      smcrfnum += 1
+      smcrfsum += float(parts[1])
+  smc_rf = smcrfsum/smcrfnum
+            
+        summary.append({'theta':theta,'lambda':lamBda,'numdeep':numdeep,'maxdeep':maxdeep,'sppTreeObsHt':stoheight, 'sppTreeExpHt':stxheight, 'smc_info':smc_info})
+        output_string  = '%d\t' % rep_plus_one
+        output_string += '%.5f\t' % theta
+        output_string += '%.5f\t' % lamBda
+        output_string += '%d\t' % numdeep
+        output_string += '%d\t' % maxdeep
+        output_string += '%.5f\t' % stoheight
+        output_string += '%.5f\t' % stxheight
+        output_string += '%.5f\t' % smc_info
+
+nsummary = len(summary)
+print('len(summary) = %d' % nsummary)
+
+if plot_theta_vs_lambda:
+    # Create thetavector and lambdavector
+    lambdavector = []
+    thetavector = []
+    for s in summary:
+        lambdavector.append('%g' % s['lambda'])
+        thetavector.append('%g' % s['theta'])
+    lambdastr = ','.join(lambdavector)
+    thetastr = ','.join(thetavector)
+else:
+    # Create halfthetavector and Tvector
+    Tvector = []
+    halfthetavector = []
+    for s in summary:
+        halftheta = s['theta']/2.0
+        T = s['sppTreeExpHt']
+        Tvector.append('%g' % T)
+        halfthetavector.append('%g' % halftheta)
+    Tstr = ','.join(Tvector)
+
+
+# Create smcInfomeans
+smcInfomeans = []
+numdeep = []
+maxdeep = []
+contour_breaks_max = None
+contour_breaks_min = None
+for s in summary:
+    smcInfomeans.append('%g' % s['smc_info'])
+    numdeep.append('%d' % s['numdeep'])
+    maxdeep.append('%d' % s['maxdeep'])
+    halfthetastr = ','.join(halfthetavector)
+     
+# Create smcmeans
+smcRFmeans = []
+for s in summary:
+    smcRFmeans.append('%g' % s['smc_rf'])
+
+    
+
+smcinfostr = ','.join(smcInfomeans)
+numdeepstr = ','.join(numdeep)
+maxdeepstr = ','.join(maxdeep)
+smcrfstr = ','.join(smcRFmeans)
+
+contour_breaks_str = calcContourBreaksStr(contour_breaks_min, contour_breaks_max)
+
+if plot_theta_vs_lambda:
+    assert False # does not work yet
+    ## Open plot-theta-lambda-template.Rmd
+    #stuff = open('plot-theta-lambda-template.Rmd', 'r').read()
+    #stuff = re.sub('__NSPECIES__', '%d' % nspecies, stuff, re.M | re.S)
+    #stuff = re.sub('__LAMBDAVECTOR__', lambdastr, stuff, re.M | re.S)
+    #stuff = re.sub('__THETAVECTOR__', thetastr, stuff, re.M | re.S)
+    #stuff = re.sub('__CONTOUR_BREAKS__', contour_breaks_str, stuff, re.M | re.S)
+    #stuff = re.sub('__NUMDEEP__', numdeepstr, stuff, re.M | re.S)
+    #stuff = re.sub('__MAXDEEP__', maxdeepstr, stuff, re.M | re.S)
+    #stuff = re.sub('__SMC_INFO__', smcinfostr, stuff, re.M | re.S)
+else:
+    # Open plot-halftheta-T-template.Rmd
+    stuff = open('plot-halftheta-T-galax-template.Rmd', 'r').read()
+    stuff = re.sub('__NSPECIES__', '%d' % nspecies, stuff, re.M | re.S)
+    stuff = re.sub('__TVECTOR__', Tstr, stuff, re.M | re.S)
+    stuff = re.sub('__HALFTHETAVECTOR__', halfthetastr, stuff, re.M | re.S)
+    stuff = re.sub('__SMC_INFO__', smcinfostr, stuff, re.M | re.S)
+    stuff = re.sub('__NUMDEEP__', numdeepstr, stuff, re.M | re.S)
+    stuff = re.sub('__MAXDEEP__', maxdeepstr, stuff, re.M | re.S)
+    stuff = re.sub('__SMCRFMEANS__', smcrfstr, stuff, re.M | re.S)
+
+outf = open('plot-bhv-info.Rmd', 'w')
+outf.write(stuff)
+outf.close()
