ae18f61ef96e27b91f36cf084765ef4dfa3f1064
amilkey1 <84950190+amilkey1@users.noreply.github.com>
2025-09-29 15:41:47 -0400

    Update td.sh

--- main-template/td.sh
+++ main-template/td.sh
@@ -1,6 +1,6 @@
 for i in rep*/smc; do
 	echo "$i"
 	cd "$i"
-	td --treefile species_trees.trees --frechet-prefix mean  --frechet-k 1000000 --frechet-n 10 --frechet-e 0.00001
+	td --treefile species_trees.trees --frechetmean --prefix mean  --frechet-k 1000000 --frechet-n 10 --frechet-e 0.00001
 	cd ../..
 done
