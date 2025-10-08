ea777de3dcf46d70e23d70abefbad335efa53324
amilkey1 <84950190+amilkey1@users.noreply.github.com>
2025-10-08 12:00:46 -0400

    Create calc-adjusted-rel-rates.py

---
+++ rep-template/rb/calc-adjusted-rel-rates.py
@@ -0,0 +1,21 @@
+nslow = 
+nfast = 
+
+total_rate = ((0.01 * nslow) + (1 * nfast)) / (nslow + nfast)
+slow_rate = (1 / total_rate) * 0.01
+faster_rate = (1 / total_rate) * 1
+
+# assert rates average to 1
+assert (slow_rate * nslow + nfast * faster_rate == nslow + nfast)
+
+rates = "relative_rates = "
+for i in range(nslow):
+	rates = rates + str(slow_rate) + " , "
+
+for i in range(nfast):
+	if i < nfast - 1:
+		rates = rates + str(faster_rate) + " , "
+	else:
+		rates = rates + str(faster_rate)
+
+print(rates)
