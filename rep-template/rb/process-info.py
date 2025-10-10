b110ce9ff23472f956cb163e273d97da5e49f1ea
amilkey1 <84950190+amilkey1@users.noreply.github.com>
2025-10-10 17:13:16 -0400

    process-info.py now creates files for all cutoff values

--- rep-template/rb/process-info.py
+++ rep-template/rb/process-info.py
@@ -1,29 +1,31 @@
 import os
 
+cutoff_values = (0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9)
+
 filename = "info.txt"
-cutoff_value = 0.8
-my_file = "remove" + str(cutoff_value) + ".sh"
-count = 1
+for value in cutoff_values:
+	cutoff_value = value
+	my_file = "remove" + str(cutoff_value) + ".sh"
+	count = 1
 
-if os.path.exists(my_file):
-	os.remove(my_file)
-	print(f"Existing file '{my_file}' removed successfully.")
+	if os.path.exists(my_file):
+		os.remove(my_file)
+		print(f"Existing file '{my_file}' removed successfully.")
 
-try:
-	with open(filename, 'r') as file_object:
-		for line in file_object:
-            # Process each line here
-            # The 'line' variable will contain the current line as a string,
-            # including the trailing newline character (\n) if present.
-			line = line.strip()
-			line = line.replace(",", "")
-			if float(line) < float(cutoff_value):
-				print(line)
-				with open (my_file, "a") as f:
-					str_to_write = "rm locus" + str(count) + ".nex" + "\n"
-					f.write(str_to_write)
-			count = count + 1
-except FileNotFoundError:
-	print(f"Error: The file '{filename}' was not found.")
-except Exception as e:
-	print(f"An unexpected error occurred: {e}")
+	try:
+		with open(filename, 'r') as file_object:
+			for line in file_object:
+            	# Process each line here
+            	# The 'line' variable will contain the current line as a string,
+            	# including the trailing newline character (\n) if present.
+				line = line.strip()
+				line = line.replace(",", "")
+				if float(line) < float(cutoff_value):
+					with open (my_file, "a") as f:
+						str_to_write = "rm locus" + str(count) + ".nex" + "\n"
+						f.write(str_to_write)
+				count = count + 1
+	except FileNotFoundError:
+		print(f"Error: The file '{filename}' was not found.")
+	except Exception as e:
+		print(f"An unexpected error occurred: {e}")
