cfd728ee16641d716d9b7964da42b2b78801357a
amilkey1 <84950190+amilkey1@users.noreply.github.com>
2025-10-12 15:21:04 -0400

    Update make-cutoff-folders.sh

--- rep-template/rb/make-cutoff-folders.sh
+++ rep-template/rb/make-cutoff-folders.sh
@@ -6,9 +6,15 @@ do
 	do
 		cp ../gene$l/locus$l.nex .
 	done
+	# copy and run the remove loci script
 	cp ../remove0.$v.sh .
 	. remove0.$v.sh
+
+	# crunch the remaining loci
 	python3 ../crunch.py
+
+	# copy and edit the proj.conf file
 	cp ../../smc/proj.conf .
+	sed -i 's#../sim/sim.nex#COMBINED.nex#g' proj.conf
 	cd ..
 done
