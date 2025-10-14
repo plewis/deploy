5a5c9a32be78dd096ffbd939d52a7054814573f0
amilkey1 <84950190+amilkey1@users.noreply.github.com>
2025-10-14 14:55:57 -0400

    Update crunch.py

--- rep-template/rb/crunch.py
+++ rep-template/rb/crunch.py
@@ -1,13 +1,24 @@
 from Bio.Nexus import Nexus
 import os
 import glob
+import re
+
+def get_number_from_file_name(s):
+	# find numbers in string
+	numbers = re.findall(r'\d+', s)
+	if numbers:
+		return int(numbers[0])
+	return 0
 
 file_list = []
 
 for value in glob.glob('*.nex'):
 	file_list.append(value)
 
-nexi = [(fname, Nexus.Nexus(fname)) for fname in file_list]
+# sort list of files according to the locus number
+sorted_file_list = sorted(file_list, key=get_number_from_file_name)
+
+nexi = [(fname, Nexus.Nexus(fname)) for fname in sorted_file_list]
 
 combined = Nexus.combine(nexi)
 combined.write_nexus_data(filename = open('COMBINED.nex', 'w'))
